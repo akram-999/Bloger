dbf77e6d2d13739d036f4f05b3027cf1a6bb2262
akram ahil <akram.ahil123@gmail.com>
2024-08-16 17:16:34 +0100

    navi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+           {/* <Route path='/blog/:id' element={<Blog />} /> */}
+           <Route path='/contact' element={<Contact />} />
+           <Route path='/login' element={<Login />} />
+           <Route path='/register' element={<Register />} />
+        </Routes>
   );
 }
 
